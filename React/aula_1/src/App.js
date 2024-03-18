1de8e1d0a93617f09c385693d19d1fa73f321557
Arthur Frantz <124110042+ArthurSkl@users.noreply.github.com>
2024-03-18 09:27:33 -0400

    parte 2

--- React/aula_1/src/App.js
+++ React/aula_1/src/App.js
@@ -2,10 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nome = "Arthur"
+
+  function sum(a,b){
+    return a+b
+  }
+
+  const url = 'https://via.placeholder.com/150'
   return (
     <div className="App">
       <h1>Ola React!</h1>
       <p>Meu primeiro app em React</p>
+      <p>Olá, {nome}</p>
+      <p>Soma: {sum(5,9)}</p>
+      <img src={url} alt="minha imagem"/> 
     </div>
   );
 }
